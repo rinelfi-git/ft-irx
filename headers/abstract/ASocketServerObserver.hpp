63a6f24d5c793ed6ea331d492d134ddc2a4bc2e5
erijania <erijania@student.42antananarivo.mg>
2025-08-23 17:17:26 +0300

    file: ASocketServerObserver.cpp

--- headers/abstract/ASocketServerObserver.hpp
+++ headers/abstract/ASocketServerObserver.hpp
@@ -1,10 +1,18 @@
 #ifndef ASOCKET_SERVER_OBSERVER_HPP
 # define ASOCKET_SERVER_OBSERVER_HPP
+# include <string>
 
-class	IRCServer;
+class	SocketServer;
 
 class	ASocketServerObserver
 {
-	
+private:
+	SocketServer&	_observable;
+public:
+	ASocketServerObserver(SocketServer& observable);
+	virtual ~ASocketServerObserver();
+	virtual void	onConnection(int fd) = 0;
+	virtual void	onData(int fd, const std::string& data) = 0;
+	virtual void	onDisconnect(int fd) = 0;
 };
 #endif
