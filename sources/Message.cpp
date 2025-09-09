5a24cc23512190310f37035361a0c90d3c318db1
telolahy <telodan9@gmail.com>
2025-09-09 16:33:47 +0300

    privmsg channel and user ok

--- sources/Message.cpp
+++ sources/Message.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include "Message.hpp"
 #include "User.hpp"
+#include <sstream>
 
 Message::Message(const User& sender, const std::string& content , const std::string& to):
 	_content(content),
@@ -19,5 +20,9 @@ const Message&	Message::to(const std::string& set)
 
 std::string	Message::toString(void) const
 {
-	return("");
+	std::ostringstream oss;
+    oss << ":" << _sender.networkId()
+        << " PRIVMSG " << _to
+        << " :" << _content;
+    return oss.str();
 }
